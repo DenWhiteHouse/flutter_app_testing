c37caa7cd55898432bf6755d5afa6de6ea24c569
aesys <fcregs10@gmail.com>
2020-02-14 12:37:22 +0100

    UI

--- lib/presentation/pages/BookPage.dart
+++ lib/presentation/pages/BookPage.dart
@@ -1,47 +1,64 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app_testing/network/book_model.dart';
 
 class BookPage extends StatelessWidget {
+  final book_model book;
+
+  BookPage(this.book);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Books'),
       ),
-      body: Center(
-        child: Column(
-          children: <Widget>[
-            Row(children: <Widget>[
-              Container(
-                //padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-                margin: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
-                child: Image.network(
-                  'https://picsum.photos/250?image=9',
-                ),
-                constraints: BoxConstraints(
-                    maxHeight: 170.0,
-                    maxWidth: 170.0,
-                    minWidth: 170.0,
-                    minHeight: 170.0),
+      body: Column(
+        children: <Widget>[
+          SizedBox( height: 180,
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Container(
+                    //padding: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+                    margin: EdgeInsets.symmetric(vertical: 10,horizontal: 10),
+                    child: Image.network(
+                      '${book.items[0].volumeInfo.imageLinks.smallThumbnail}',
+                      fit: BoxFit.fill,
+                    ),
+                    constraints: BoxConstraints(
+                        maxHeight: 180.0,
+                        maxWidth: 180.0,
+                        minWidth: 170.0,
+                        minHeight: 170.0),
 
-              ),
-              Container(
-                child: Column(
-                  children: <Widget>[
-                    Text('Titolo',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 40
-                    )),
-                    SizedBox(height: 20,),
-                    Text('Pagine:'),
-                    SizedBox(height: 20,),
-                    Text('Descrizione:')
-                  ],
-                )
-              )
-            ])
-          ],
-        ),
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: <Widget>[
+                        AutoSizeText('${book.items[0].volumeInfo.title}',
+                            maxLines: 2,
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 25
+
+                            )),
+                        SizedBox(height: 20,),
+                        Text('N.Pages: ${book.items[0].volumeInfo.pageCount}'),
+                        SizedBox(height: 20,),
+                        AutoSizeText('${book.items[0].volumeInfo.description}',
+
+                            style: TextStyle(
+                                fontSize: 10
+                            ))
+                      ],
+                    ),
+                  )
+                ]),
+          )
+        ],
       ),
     );
   }
