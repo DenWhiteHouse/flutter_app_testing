9c15711d7f4d6677ef780ef7f4ad4fb3544ad83d
aesys <fcregs10@gmail.com>
2020-02-17 11:48:54 +0100

    UI

--- lib/presentation/bloc/book_bloc.dart
+++ lib/presentation/bloc/book_bloc.dart
@@ -9,17 +9,13 @@ class BookBloc extends Bloc<BookEvent, BookState> {
   @override
   BookState get initialState => BookInitial();
 
-  // Under the hood, the Bloc library works with regular Dart Streams.
-  // The "async*" makes this method an async generator, which outputs a Stream
   @override
   Stream<BookState> mapEventToState(
       BookEvent event,
       ) async* {
     print('Sono quiiii');
-    // Distinguish between different events, even though this app has only one
     if (event is GetBook) {
       print('Sono quiiii2');
-      // Outputting a state from the asynchronous generator
       yield BookLoading();
       final book = await BookApiProvider().getBook();
       yield BookLoaded(book);
